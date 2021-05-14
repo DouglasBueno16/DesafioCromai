649a1edcb7c05780f355ffd16a64a09ebd6b64ed
Douglas <69056232+DouglasBueno16@users.noreply.github.com>
2021-05-13 21:04:39 -0300

    Update teste_leitura_arq_if.sh
    Update com comentários pertinentes ao código

--- teste_leitura_arq_if.sh
+++ teste_leitura_arq_if.sh
@@ -4,9 +4,9 @@
 # No momento do teste a variável pid do continha o valor 670.
 cat pid.txt|while read line  # Abre o arquivo pid.txt e realiza a leitura das linhas armazendo os
 do
-    if [ $line -eq 670 ]
+    if [ $line -eq 670 ]  # Condicional if verifica se o valor eh igual a 670
     then
-        echo $line
-        echo "Parabens"
-    fi
-done
+        echo $line  # imprime no terminal o valor da linha
+        echo "Parabens"  # imprime a mensagem
+    fi  # fecha o bloco if
+done  # encerra o programa
