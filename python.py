40cc55738655684aa815dc70ac9914ba83262e5d
Douglas <69056232+DouglasBueno16@users.noreply.github.com>
2021-05-16 19:16:55 -0300

    Inclusão de arquivos
    Inclusão dos arquivos python.py e shell.sh

--- python.py
+++ python.py
@@ -2,23 +2,39 @@
 Script Python que abre um arquivo de nome "pid", fazendo a leitura e escrevendo seu PID dentro do
 arquivo.
 PID - Identificador de Processo
+
+- Início do Programa
+    Utiliza a biblioteca os, para encontrar o valor do pid do programa em python3
+    Converte o valor do pid encontrado para o formato string
+
+- Escrita no arquivo
+    Cria um arquivo de nome pid.txt
+    Escreve o valor da variável pid no arquivo
+
+- Loop while
+    Inicia um contador com valor 0
+    Verifica se o valor do contador é menor que 3, caso seja maior -> fim do programa
+    Imprime a mensagem no terminal do computador
+    Incrementa uma unidade no valor do contador
+    Aguarda x segundos antes do próxima iteração
 """
-# ==== Importando bibliotecas necessárias ==== #
+# ==== Importando bibliotecas ==== #
 import os
 from time import sleep
 
 # ==== Início do Programa ==== #
-pid = str(os.getpid())  # Variável para armazenar o valor do PID já convertida para string
-print(f'O pid é : {pid}')  # Impressão para verificar o valor do pid
+pid = str(os.getpid())
 
-with open('pid.txt',mode='w' ) as arquivo:  # Abre o arquivo pid, como arquivo -> facilita a programação
-    i = 0  # Inicia a variável de contagem i com valor zero
-    arquivo.write(pid)  # Escreve a variável pid no arquivo txt
-    arquivo.write('\n')  # Pula uma linha no arquivo
+# ==== Escrita no arquivo ==== #
+with open('pid.txt', mode='w') as arquivo:
+    arquivo.write(pid)
+    arquivo.write('\n')
 
-while (i < 3):
-    print('2: I am alive')        
+# ==== Loop while ==== #
+i = 0
+while i < 3:
+    print('2: I am alive')
     i = i + 1
-    sleep(2)  # Aguarda 2 segundos antes da próxima execução
-    
+    sleep(2)
+
 print("2: I gonna die now, bye")  # Final do programa
---
+++ shell.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Shell Script - Linux
+# Verifica se o arquivo pid.txt existe, caso não encontre o arquivo, armazena o erro em cromai.log
+# Caso encontre o arquivo, faz a verificação do pid e se este pid é de um arquivo em python3
+# Caso o pid seja de um arquivo python3, imprime a mensagem
+# Caso não existe um pid para python3, imprime outra mensagem e executa o programa python.py
+# Comando echo -> imprime a mensagem dentro do terminal
+# Comando sleep -> pausa o programa por x segundos
+
+while true; do
+    if [ -e pid.txt ]; then
+        while read line
+        do
+            if [ "$( ps -p $line -o comm= )" = "python3" ];
+            then
+                echo "1: It is alive"
+                sleep 5
+            else
+                echo "1: It is dead"
+                sleep 5
+                python3 python.py 
+            fi
+        done < pid.txt
+    else
+        date >> "cromai.log"
+        echo "Arquivo nao existe" >> "cromai.log"
+        break
+    fi
+done
